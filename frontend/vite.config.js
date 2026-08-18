38edf96b720b52c35a5c519b8c39c399f91de142
brucewerk <brucewerk@gmail.com>
2026-08-18 00:22:35 -0300

    test: verificar deploy automatico

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // ← IMPORTANTE: garante que os assets sejam buscados na raiz
   server: {
     port: 5173,
     proxy: {
